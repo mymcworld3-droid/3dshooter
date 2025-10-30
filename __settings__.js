82541d1e8c6c16ed87e90f50c3e1d3dfcc79fc49
mymcworld3-droid <mymcworld3@gmail.com>
2025-10-30 18:27:28 +0800

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2351155.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 260994155, 261037620 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/260749781/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/260749782/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/260749780/1/ammo.js', 'preload' : true},
+];
